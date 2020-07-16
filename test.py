cd582dfabb00cbd39a053d6cceb1bd849cf0f309
pranjalsrajput <pranjalsrajput@gmail.com>
2020-07-16 20:38:58 +0200

    first draft of dashboard

--- test.py
+++ test.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask_table import Table, Col
+from flask import Markup
 
 app = Flask(__name__)
 
@@ -18,7 +19,7 @@ class Items(object):
 @app.route('/')
 @app.route('/home')
 def sayHello():
-    items = [Items("Pranjal", "Boy"), Items("Shivangi", "Girl")]
+    items = [Items(Markup('<img src="DurationStats.png">'), "Boy"), Items("DurationStats.png", "Girl")]
     table = Item_Table(items)
     print(table.__html__())
     return table.__html__()
@@ -29,9 +30,24 @@ def about():
     return 'About me'
 
 
+def convertExcelToJson(filename, sheetname):
+    import pandas
+    import json
+
+    excel_data_df = pandas.read_excel(filename, sheet_name=sheetname)
+
+    json_str = excel_data_df.to_json(orient='records')
+
+    print('Excel Sheet to JSON:\n', json_str)
+    # json.dump(json_str, "outputJson")
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    # app.run(debug=True)
 
     # items = [Items("Pranjal", "Boy"), Items("Shivangi", "Girl")]
     # table = Item_Table(items)
     # print(table.__html__())
+    convertExcelToJson(
+        '/home/pranjal/Documents/PythonProjects/Large-Video-Dataset-Annotation-Strategies/Marathon_Eindhoven_velo_method.xlsx',
+        'Full marathon sheet')
